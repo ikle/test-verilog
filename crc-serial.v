5be36484fd5322049b68aa402cb6ade3b241d7e5
Alexei A. Smekalkine <ikle@ikle.ru>
2020-09-08 20:18:54 +0300

    crc-serial: add module description and more samples

--- crc-serial.v
+++ crc-serial.v
@@ -6,15 +6,22 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+/*
+ * Serial CRC module, CRC-32 by default
+ *
+ * Polinomial specified in LSB-first order (reversed).
+ *
+ * CRC-4-ITU	(4,  'hC)
+ * CRC-16-CCITT	(16, 'h8408)
+ * CRC-32	(32, 'hEDB88320)
+ * CRC-32C	(32, 'h82F63B78)
+ */
 module crc_serial #(
-	parameter W = 32, P = 'hEDB88320  // LSB-first CRC32 code
+	parameter W = 32, P = 'hEDB88320  // LSB-first CRC-32 code
 ) (
 	input reset, enable, clk, d,
 	output reg [W-1:0] q
 );
-//	parameter W = 4;
-//	parameter P = 'hC;		// LSB CRC-4-ITU
-
 	always @(posedge reset, posedge clk)
 		if (reset)
 			q <= ~0;
